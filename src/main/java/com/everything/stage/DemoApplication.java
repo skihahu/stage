7f40f181200b69c09d74eba40c57f4aae987e9d3
kihahu <sam@tala.co>
2018-10-23 11:42:20 +0300

    Initial import

---
+++ src/main/java/com/everything/stage/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.everything.stage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
